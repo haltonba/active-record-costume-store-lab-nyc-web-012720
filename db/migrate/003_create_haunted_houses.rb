74e8376c710ce0af97ecc95480d76a0f5c5998a6
Halton Bagley <haltonba94@gmail.com>
2020-02-16 17:24:33 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -6,7 +6,9 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.1]
       t.string :theme
       t.float :price
       t.boolean :family_friendly
-      t.date
+      t.datetime :opening_date
+      t.datetime :closing_date
+      
     end
   end
 end
